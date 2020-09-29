d400958efb8a49cd9b6050fc03783b218891d165
Liza Babu <lizza.babu@gmail.com>
2020-09-29 01:59:27 -0700

    Add Bubble Sort implementation, update README accordingly

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,11 +1,54 @@
-#include <stdio.h>
 
-static void sort(void)
+#include <stdio.h> 
+
+#define MAX_LEN 100
+
+static void swap(int *x, int *y)
+{ 
+	int tmp = *x; 
+	*x = *y; 
+	*y = tmp; 
+} 
+
+static void bubble_sort(int *arr, int len)
+{ 
+	int i, j;
+
+	for (i = 0; i < len - 1; i++)
+		for (j = 0; j < len - 1; j++)  
+			if (arr[j] > arr[j + 1]) 
+				swap(&arr[j], &arr[j + 1]); 
+}
+
+static void print_array(int *arr, int len)
 {
-    // TODO: add bubble sort algorithm here
+	int i;
+
+	for (i = 0; i < len; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 }
 
 int main()
 {
-    return 0;
-}
+	int arr[MAX_LEN], len, i;
+	
+	printf("What's the length of the array? Maximum lenght is %d\n", MAX_LEN);
+	scanf("%d", &len);
+
+	printf("Gimme the %d elements\n", len);
+	for (i = 0; i < len; i++) {
+	scanf("%d", &arr[i]);
+	}
+
+	printf("Nonsorted array: ");
+	print_array(arr, len);
+
+	bubble_sort(arr, len);
+
+	printf("Sorted array: ");
+	print_array(arr, len);
+
+	return 0; 
+} 
